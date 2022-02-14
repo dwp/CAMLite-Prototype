5b231be1233e53984533a53526c98477d35fa18d
nsuska <97541053+nsuska@users.noreply.github.com>
2022-02-14 17:57:52 +0000

    first push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,18 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
   window.MOJFrontend.initAll()
 })
+
+new MOJFrontend.ButtonMenu({
+  container: $("#navi-search"),
+  mq: "(min-width: 200em)",
+  buttonText: "Search",
+  menuClasses: "moj-button-menu__wrapper--right",
+});
+
+new MOJFrontend.ButtonMenu({
+  container: $("#cases-list-menu"),
+  mq: "(min-width: 200em)",
+  buttonText: "Menu",
+  buttonClasses:
+    "govuk-button--secondary moj-button-menu__toggle-button--secondary",
+});
