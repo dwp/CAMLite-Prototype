146101418a8b88bce6f14ce8c71597ca1c4d9d30
nsuska <97541053+nsuska@users.noreply.github.com>
2022-03-24 16:37:37 +0000

    first push

--- app/routes/alphaTeamleader.js
+++ app/routes/alphaTeamleader.js
@@ -35,5 +35,23 @@ router.post('/assigned-cases-confirmation-b', function(req, res) {
                                   }
                                 });
 
+    //changing records
+                router.post('/change-records', function (req, res) {
+                  const changerecords = req.session.data['change-records']
+                  console.log(changerecords)
+                  if (changerecords === 'status') {
+                      res.redirect('/alpha/camlite/cases/change-status-bulk')
+
+                  } else if (changerecords === 'duedate') {
+                        res.redirect('/alpha/camlite/cases/change-due-date')
+
+                  } else if (changerecords === 'resolution') {
+                    res.redirect('/alpha/camlite/cases/change-resolution')
+
+            }
+            });
+
+
+
 
 module.exports = router
